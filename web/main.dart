83db907cc190ec36c572db51851992796922eca2
redsolver <info@redsolver.net>
2020-10-06 15:37:48 +0200

    Save files in chunks on multiple portals

--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,25 @@
 import 'dart:convert';
 import 'dart:html';
+import 'dart:math';
 import 'dart:typed_data';
 import 'package:cryptography/cryptography.dart';
+
 import 'package:http/http.dart' as http;
+
 import 'package:http_parser/http_parser.dart';
+
+import 'package:uuid/uuid.dart';
+
+const chunkSize = 1000 * 1000 * 16; // 16 MB
+
+List<String> publicPortals = [
+  'https://siasky.net', // FAST and CORS
+  'https://skyportal.xyz', // FAST and CORS
+  // 'https://sialoop.net',// NO CORS
+  // 'https://skydrain.net', // SLOW
+  // 'https://siacdn.com',// NO CORS
+  'https://skynethub.io', // FAST and CORS
+];
 
 void main() {
   /*  try { */
@@ -14,11 +30,20 @@ void main() {
   }
 
   if (hash.isNotEmpty) {
-    setState('Downloading file...');
+    if (hash.startsWith(RegExp(r'[0-9]'))) {
+      setState('Redirecting to old version...');
+
+      window.location.href =
+          'https://siasky.net/CACxu3qIoxiXQdyDBmrcS7dkC4sGzz4NrXpReKnehKEwFQ/index.html#$hash';
+      return;
+    }
+
+    // hash = hash.substring(1);
+    setState('Downloading file index...');
 
     final lengthSep = hash.indexOf('-');
 
-    final mdLength = int.parse(hash.substring(0, lengthSep));
+    final version = hash.substring(0, lengthSep);
 
     hash = hash.substring(lengthSep + 1);
 
@@ -30,7 +55,10 @@ void main() {
     print(skylink);
     print(key); */
 
-    downloadAndDecrypt(skylink, key, mdLength);
+    downloadAndDecrypt(
+      skylink,
+      key,
+    );
   }
   //var input = window.document.querySelector('#upload');
 
@@ -46,28 +74,24 @@ void main() {
     File file = files.first;
 
     // file.slice()
-
-    final reader = FileReader();
-
-    reader.onLoad.listen((progressEvent) {
-      // print("file read");
-
-      //print(reader.result.runtimeType);
-
-      encryptAndUpload(file, reader.result);
-    });
-    reader.onError.listen((event) {
-      print("error ${reader.error.name}");
-    });
-
-    /* reader.onProgress.listen((event) { 
-    }); */
-    reader.readAsArrayBuffer(file);
+    encryptAndUpload(
+      file,
+    );
   });
-/*   } catch (e, st) {
-    print(e);
-    print(st);
-  } */
+}
+
+Stream<List<int>> getStreamOfFile(File file) async* {
+  final reader = FileReader();
+
+  int start = 0;
+  while (start < file.size) {
+    final end = start + chunkSize > file.size ? file.size : start + chunkSize;
+    final blob = file.slice(start, end);
+    reader.readAsArrayBuffer(blob);
+    await reader.onLoad.first;
+    yield reader.result;
+    start += chunkSize;
+  }
 }
 
 void setState(String s) {
@@ -79,63 +103,82 @@ class TrustedNodeValidator implements NodeValidator {
   bool allowsAttribute(element, attributeName, value) => true;
 }
 
-final String skynetPortal = 'https://siasky.net';
-String get skynetPortalUploadUrl => '$skynetPortal/skynet/skyfile';
+void downloadAndDecrypt(
+  String skylink,
+  String key,
+) async {
+  print(skylink);
 
-void downloadAndDecrypt(String skylink, String key, int mdLength) async {
-  final res = await http.get('$skynetPortal/$skylink');
+  final portal = 'https://siasky.net';
+
+  final res = await http.get('$portal/$skylink');
 
   final cryptParts = base64.decode(key);
 
-  // Choose the cipher
   final cipher = CipherWithAppendedMac(aesCtr, Hmac(sha256));
 
-  // Choose some 256-bit secret key
   final secretKey = SecretKey(cryptParts.sublist(0, 32));
 
-  // Choose some unique (non-secret) nonce (max 16 bytes).
-  // The same (secretKey, nonce) combination should not be used twice!
   final nonce = Nonce(cryptParts.sublist(32, 32 + 16));
 
-  // print(key);
+  setState('Decrypting file index...');
 
-/*     final metadata = {
-    'filename': file.name,
-    'type': file.type,
-  };
-
-  final md = json.encode(metadata);
-
-  final mdBytes = utf8.encode(md); */
-
-  setState('Decrypting file...');
-
-  final decrypted = await cipher.decrypt(
+  final decryptedChunkIndex = await cipher.decrypt(
     res.bodyBytes,
     secretKey: secretKey,
     nonce: nonce,
   );
 
-  final contentBytes = decrypted.sublist(mdLength);
+  final Map chunkIndex = json.decode(utf8.decode(decryptedChunkIndex));
 
-  //print(contentBytes.sublist(0, 10));
+  print(chunkIndex);
 
-  final metadataBytes = decrypted.sublist(0, mdLength);
+  final Map metadata = chunkIndex['metadata'];
 
-  final mdText = utf8.decode(metadataBytes);
+  List<Uint8List> chunks = [];
 
-  // print('%$mdText%');
+  int i = 0;
 
-  final Map metadata = json.decode(mdText);
+  final int totalChunks = metadata['totalchunks'];
+  setState('Downloading and decrypting chunk 1 of $totalChunks...');
 
-  final blob = Blob([contentBytes], metadata['type']);
+  for (final chunkSkylink in chunkIndex['chunks']) {
+    i++;
 
-  downloadBlob(blob, metadata['filename']);
+    final currentI = i;
+
+    http
+        .get(
+      '$portal/$chunkSkylink',
+    )
+        .then((chunkRes) async {
+      final decryptedChunk = await cipher.decrypt(
+        chunkRes.bodyBytes,
+        secretKey: secretKey,
+        nonce: nonce,
+      );
+
+      while (chunks.length < currentI - 1) {
+        await Future.delayed(Duration(milliseconds: 20));
+      }
+
+      chunks.add(decryptedChunk);
+
+      if (currentI == totalChunks) {
+        final blob = Blob(chunks, metadata['type']);
+
+        downloadBlob(blob, metadata['filename']);
+      } else {
+        setState(
+            'Downloading and decrypting chunk ${currentI + 1} of $totalChunks...');
+      }
+    });
+
+    await Future.delayed(Duration(seconds: 1));
+  }
 }
 
 void downloadBlob(Blob blob, String filename) {
-  // Create an object URL for the blob object
-
   setState('<a id="downloadLink">Save File ${filename}</a>');
 
   window.document.querySelector("#downloadLink")
@@ -143,25 +186,32 @@ void downloadBlob(Blob blob, String filename) {
     ..setAttribute('download', filename);
 }
 
-void encryptAndUpload(File file, Uint8List result) async {
+int i = 0;
+
+Stream<List<int>> encryptStreamInBlocks(Stream<List<int>> source,
+    CipherWithAppendedMac cipher, SecretKey secretKey, Nonce nonce) async* {
+  i = 0;
+  int internalI = 0;
+  // Wait until a new chunk is available, then process it.
+  await for (var chunk in source) {
+    internalI++;
+    while (i < internalI - 1) {
+      await Future.delayed(Duration(milliseconds: 20));
+    }
+    yield await cipher.encrypt(
+      chunk,
+      secretKey: secretKey,
+      nonce: nonce,
+    );
+  }
+  print('done');
+}
+
+void encryptAndUpload(
+  File file,
+) async {
+  setState('Encrypting and uploading file...');
   // print(file.type);
-
-  setState('Encrypting file...');
-
-  //final unencryptedContentBytes = result.codeUnits;
-
-  final metadata = {
-    'filename': file.name,
-    'type': file.type,
-  };
-
-  final md = json.encode(metadata);
-
-  final mdBytes = utf8.encode(md);
-
-  int mdL = mdBytes.length;
-
-  final unencryptedBytes = [...mdBytes, ...result];
 
   // Choose the cipher
   final cipher = CipherWithAppendedMac(aesCtr, Hmac(sha256));
@@ -173,53 +223,118 @@ void encryptAndUpload(File file, Uint8List result) async {
   // The same (secretKey, nonce) combination should not be used twice!
   final nonce = Nonce.randomBytes(16);
 
-  
+  final totalChunks = (file.size / (chunkSize + 32)).abs().toInt() + 1;
 
-  // Encrypt
-  final encrypted = await cipher.encrypt(
-    unencryptedBytes,
+  final metadata = {
+    'filename': file.name,
+    'type': file.type,
+    'chunksize': chunkSize,
+    'totalchunks': totalChunks,
+  };
+
+/*   final md = json.encode(metadata);
+
+  final mdBytes = utf8.encode(md); */
+
+  //int mdL = mdBytes.length;
+
+  // Metadata start (mdL + 32)
+
+  final stream =
+      encryptStreamInBlocks(getStreamOfFile(file), cipher, secretKey, nonce);
+
+  final chunkSkylinks = await uploadChunkedStreamToSkynet(
+      file.size, stream /* .asBroadcastStream() */
+
+      );
+
+  print(chunkSkylinks);
+
+  setState('Encrypting and uploading chunk index...');
+
+  final links = await cipher.encrypt(
+    utf8.encode(json.encode({
+      'chunks': chunkSkylinks,
+      'metadata': metadata,
+    })),
     secretKey: secretKey,
     nonce: nonce,
   );
 
-  final skylink = await uploadFileToSkynet(
-      file.size, encrypted, MediaType('application', 'octet-stream'));
+  final skylink = await uploadFileToSkynet(links);
 
-  print(skylink);
+  // Encrypt
 
   final secret =
       base64.encode([...(await secretKey.extract()), ...nonce.bytes]);
 
   final link =
-      '${window.location.protocol}//${window.location.host}${window.location.pathname}#$mdL-$skylink+$secret';
+      '${window.location.protocol}//${window.location.host}${window.location.pathname}#a-$skylink+$secret';
 
   setState('Secure Download Link for ${file.name}: <a href="$link">$link</a>');
 }
 
-Future<String> uploadFileToSkynet(
-    int fileSize, Uint8List bytes, MediaType contentType) async {
-  var stream = new http.ByteStream.fromBytes(bytes);
+String getRandomPortal() {
+  return publicPortals[Random().nextInt(publicPortals.length)];
+}
 
-  var uri = Uri.parse(skynetPortalUploadUrl);
+Future<List<String>> uploadChunkedStreamToSkynet(
+    int fileSize, Stream<List<int>> byteUploadStream) async {
+  final totalChunks = (fileSize / (chunkSize + 32)).abs().toInt() + 1;
+
+  final uploaderFileId = Uuid().v4();
+  print('send $uploaderFileId');
+
+  setState(
+      'Encrypting and uploading first chunk... $totalChunks Chunks total (16 MB each)');
+
+  List<String> skylinks = [];
+
+  // TODO Parallel chunk uploading to multiple portals
+  await for (final chunk in byteUploadStream) {
+    String skylink;
+
+    while (skylink == null) {
+      try {
+        skylink = await uploadFileToSkynet(chunk);
+
+        if ((skylink ?? '').isEmpty) throw Exception('oops');
+      } catch (e, st) {
+        print(e);
+        print(st);
+        print('retry');
+      }
+    }
+
+    skylinks.add(skylink);
+    i++;
+
+    setState(
+        'Encrypting and uploading file... $i/$totalChunks Chunks uploaded (16 MB each)');
+  }
+  return skylinks;
+}
+
+Future<String> uploadFileToSkynet(List<int> chunk) async {
+  var byteStream = new http.ByteStream.fromBytes(chunk);
+
+  var uri = Uri.parse(getRandomPortal() + '/skynet/skyfile');
+
+  print(uri);
 
   var request = new http.MultipartRequest("POST", uri);
+
   var multipartFile = new http.MultipartFile(
     'file',
-    stream,
-    fileSize,
+    byteStream,
+    chunk.length,
     filename: 'blob',
-    /*  contentType: MediaType(primaryType, subType) */
-    contentType: contentType,
+    contentType: MediaType('application', 'octet-stream'),
   );
-  //contentType: new MediaType('image', 'png'));
 
   request.files.add(multipartFile);
 
-  setState('Uploading encrypted file...');
-
   var response = await request.send();
-
-  setState('Uploaded encrypted file!');
 
   if (response.statusCode != 200) {
     throw Exception('HTTP ${response.statusCode}');
@@ -231,16 +346,5 @@ Future<String> uploadFileToSkynet(
 
   if (resData['skylink'] == null) throw Exception('Skynet Upload Fail');
 
-  final skylink = resData['skylink'];
-
-  return skylink;
-}
-
-Future<void> uploadFile() async {
-/*   print('Encrypted: $encrypted');
-
-  // Decrypt
-
-
-  print('Decrypted: $decrypted'); */
+  return resData['skylink'];
 }
